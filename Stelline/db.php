be259b352420d83f033c4df5f5a908f0dff18b48
Antonio Belculfinè <antoniobcf@hotmail.it>
2018-08-15 20:45:29 +0200

    hgyg

--- Stelline/db.php
+++ Stelline/db.php
@@ -5,8 +5,7 @@ $conn = new mysqli("localhost", "root", "", "Database");
 $comandoSQL = "select name from tbl_restaurant where id = 1";
 
 $ris = $conn -> query($comandoSQL);
-
-$riga = &ris -> fetch_assoc();
+$riga = $ris -> fetch_assoc();
 
 echo "Il nome e': ".$riga['name']."!";
 ?>
