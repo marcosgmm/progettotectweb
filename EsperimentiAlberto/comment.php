7a48df9850246e49920fd877925ddcee0711a7ea
Alberto Cherobin <albertocherobin@gmail.com>
2018-08-16 10:02:06 +0100

    modifiche

--- EsperimentiAlberto/comment.php
+++ EsperimentiAlberto/comment.php
@@ -1,10 +1,10 @@
 <?php
-    if(isset($_GET['nome'])){
-        $name = $_GET['nome'];
-    }
+
+
+
     $testo = $_POST['commento'];
     $file = fopen("home.html", "a+");
-    fwrite($file, $name."<br>".$testo."<br><br>");
+    fwrite($file,"<br>".$testo."<br><br>");
     fclose($file);
 
     $ritorno = $_SERVER["HTTP_REFERER"];
