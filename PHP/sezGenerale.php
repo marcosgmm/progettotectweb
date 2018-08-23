1952a5bd961802f0577a04fa7c5bbd3b0e0de8f9
Alberto Cherobin <albertocherobin@gmail.com>
2018-08-23 15:15:14 +0100

    articoli multipli quasi pronto

--- PHP/sezGenerale.php
+++ PHP/sezGenerale.php
@@ -8,12 +8,32 @@
 
         $conn = new mysqli("localhost","root","", "prova");
 
-        $SQLtitolo = "select titolo from ". $citta ." where sezione ='" . $sezione ."'";
+        $SQL = "select * from ". $citta ." where sezione ='" . $sezione ."' ORDER BY id ASC LIMIT 1";
+        $RIS = $conn -> query($SQL);
+
+    /*  $SQLtitolo = "select titolo from ". $citta ." where sezione ='" . $sezione ."'";
         $SQLtesto = "select testo from ". $citta ." where sezione ='" . $sezione ."'";
         $SQLimg = "select img from ". $citta ." where sezione ='" . $sezione ."'";
-        $SQLalt = "select alt from ". $citta ." where sezione ='" . $sezione ."'";
+        $SQLalt = "select alt from ". $citta ." where sezione ='" . $sezione ."'";  */
 
-        $risultatoTitolo = $conn -> query($SQLtitolo);
+
+    while($riga = $RIS -> fetch_assoc()){
+            $titolo = $riga['titolo'];
+            $testo =  $riga['testo'];
+            $img = $riga['img'];
+            $alt = $riga['alt'];
+            $id = $riga['id'];
+            $articolo = str_replace('$TITOLO$', $titolo, $articolo);
+            $articolo = str_replace('$TESTO$', $testo, $articolo);
+            $articolo = str_replace('$URL$', $img, $articolo);
+            $articolo = str_replace('$ALT$', $alt, $articolo);
+            echo $articolo;
+            $id = $id + 1;
+            $SQL2 = "select * from ". $citta ." where sezione ='" . $sezione ."'AND id = '". $id ."'";
+            $RIS = $conn -> query($SQL2);
+        }
+
+     /*   $risultatoTitolo = $conn -> query($SQLtitolo);
         $risultatoTesto = $conn -> query($SQLtesto);
         $risultatoImg = $conn -> query($SQLimg);
         $risultatoAlt = $conn -> query($SQLalt);
@@ -34,6 +54,6 @@
             echo $articolo;
         } else{
             echo "false";
-        }
+        }  */
 
 ?>
