e05f586b81e939a6d5f6655e3a6dab3ee31fa7e8
Alberto Cherobin <albertocherobin@gmail.com>
2018-08-25 16:06:56 +0100

    aSASAS

--- PHP/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-        session_start();
-        unset($_SESSION['name']);
-        unset($_SESSION['email']);
-        unset($_SESSION['password']);
-        unset($_SESSION['cognome']);
-        session_destroy();
-        header("Location: capoHome.php");
-?>
